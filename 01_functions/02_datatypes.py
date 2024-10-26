67c8342ee2e35246fd062a228d4f32687fe8beee
mlbala <imbkrishnaa@gmail.com>
2024-10-27 00:24:08 +0530

    float datatype added

--- 01_functions/02_datatypes.py
+++ 01_functions/02_datatypes.py
@@ -17,9 +17,24 @@
 # We need to convert input from default string to int
 # z = int(x) + int(y)
 
-x = int(input("what's x? "))
-y = int(input("what's y? "))
+# x = int(input("what's x? "))
+# y = int(input("what's y? "))
 
-# z = x + y
+# # z = x + y
 
-print(x + y)
+# print(x + y)
+
+## fload data type
+# round(number[, ndigits])
+
+x = float(input("what's x? "))
+y = float(input("what's y? "))
+
+# z = round(x + y) # nearest integer
+
+z = x / y
+
+# z = round(x / y, 2)
+# print(z)
+# print(f"{z:,})
+print(f"{z:.3f}")
