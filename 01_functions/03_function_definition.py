11108b362ae7e833e0804911a0f35428618293fb
mlbala <imbkrishnaa@gmail.com>
2024-10-27 00:47:27 +0530

    function definition

---
+++ 01_functions/03_function_definition.py
@@ -0,0 +1,26 @@
+# Fucntion definition
+
+# def hello():
+#     print("Hello")
+
+# name = input("What's your name? ")
+# hello()
+# print(name)
+
+
+# def hello(to="world"):
+#     print("Hello", to)
+
+# hello()
+# name = input("What's your name? ")
+# hello(name)
+
+def main():
+    name = input("What's your name? ")
+    hello()
+
+
+def hello(to="world"):
+    print("hello", to)
+
+main()
